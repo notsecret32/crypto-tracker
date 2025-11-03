345627169322d9741b1ca42edab6abcbc91abde3
notsecret32 <43242733+notsecret32@users.noreply.github.com>
2025-11-04 01:05:58 +0300

    feat: improved sty;e of header title

--- src/features/home/components/header.tsx
+++ src/features/home/components/header.tsx
@@ -5,7 +5,7 @@ import { CoinSearch } from "./coin-search";
 export const HomeHeader = () => {
   return (
     <div className="flex justify-between items-center border-b p-4">
-      <h1>Crypto Tracker</h1>
+      <h1 className="font-bold text-xl">Crypto Tracker</h1>
       <div className="flex gap-4">
         <CoinSearch />
         <ThemeSwitch />
